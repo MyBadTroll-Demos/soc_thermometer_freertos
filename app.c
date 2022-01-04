9a28728f9795664739097ad0babaead2656e068f
Justin Reina <justinmreina@gmail.com>
2022-01-03 16:49:37 -0800

    (C)    syntax updates

--- app.c
+++ app.c
@@ -1,32 +1,29 @@
-/***************************************************************************//**
+/*********************************************************************************************************************************//**
  * @file
  * @brief Core application logic.
- *******************************************************************************
+ *************************************************************************************************************************************
  * # License
  * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
- *******************************************************************************
+ *************************************************************************************************************************************
  *
  * SPDX-License-Identifier: Zlib
  *
  * The licensor of this software is Silicon Laboratories Inc.
  *
- * This software is provided 'as-is', without any express or implied
- * warranty. In no event will the authors be held liable for any damages
- * arising from the use of this software.
+ * This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any
+ * damages arising from the use of this software.
  *
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
+ * Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and
+ * redistribute it freely, subject to the following restrictions:
  *
- * 1. The origin of this software must not be misrepresented; you must not
- *    claim that you wrote the original software. If you use this software
- *    in a product, an acknowledgment in the product documentation would be
- *    appreciated but is not required.
- * 2. Altered source versions must be plainly marked as such, and must not be
- *    misrepresented as being the original software.
+ * 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use
+ *    this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+ *
  * 3. This notice may not be removed or altered from any source distribution.
  *
- ******************************************************************************/
+ ************************************************************************************************************************************/
 #include <stdbool.h>
 #include "em_common.h"
 #include "sl_status.h"
@@ -39,13 +36,14 @@
 #include "gatt_db.h"
 #ifdef SL_COMPONENT_CATALOG_PRESENT
 #include "sl_component_catalog.h"
-#endif // SL_COMPONENT_CATALOG_PRESENT
+#endif 																		/* SL_COMPONENT_CATALOG_PRESENT							*/
 #ifdef SL_CATALOG_CLI_PRESENT
 #include "sl_cli.h"
-#endif // SL_CATALOG_CLI_PRESENT
+#endif 																		/* SL_CATALOG_CLI_PRESENT								*/
 #include "sl_sensor_rht.h"
 #include "sl_health_thermometer.h"
 #include "app.h"
+
 
 // Connection handle.
 static uint8_t app_connection = 0;
@@ -62,38 +60,47 @@ static sl_simple_timer_t app_periodic_timer;
 // Periodic timer callback.
 static void app_periodic_timer_cb(sl_simple_timer_t *timer, void *data);
 
-/**************************************************************************//**
+
+/*********************************************************************************************************************************//**
  * Application Init.
- *****************************************************************************/
-SL_WEAK void app_init(void)
-{
+ ************************************************************************************************************************************/
+SL_WEAK void app_init(void) {
+
   app_log_info("health thermometer initialised\n");
+
   // Init temperature sensor.
   sl_sensor_rht_init();
+
+  return;
 }
 
+
 #ifndef SL_CATALOG_KERNEL_PRESENT
-/**************************************************************************//**
+/*********************************************************************************************************************************//**
  * Application Process Action.
- *****************************************************************************/
-SL_WEAK void app_process_action(void)
-{
+ ************************************************************************************************************************************/
+SL_WEAK void app_process_action(void) {
+
   /////////////////////////////////////////////////////////////////////////////
   // Put your additional application code here!                              //
   // This is called infinitely.                                              //
   // Do not call blocking functions from here!                               //
   /////////////////////////////////////////////////////////////////////////////
+
+  return;
 }
 #endif
 
-/**************************************************************************//**
+
+/*********************************************************************************************************************************//**
  * Bluetooth stack event handler.
  * This overrides the dummy weak implementation.
  *
  * @param[in] evt Event coming from the Bluetooth stack.
- *****************************************************************************/
-void sl_bt_on_event(sl_bt_msg_t *evt)
-{
+ ************************************************************************************************************************************/
+void sl_bt_on_event(sl_bt_msg_t *evt) {
+
+  //Locals
   sl_status_t sc;
   bd_addr address;
   uint8_t address_type;
@@ -101,9 +108,8 @@ void sl_bt_on_event(sl_bt_msg_t *evt)
 
   // Handle stack events
   switch (SL_BT_MSG_ID(evt->header)) {
-    // -------------------------------
-    // This event indicates the device has started and the radio is ready.
-    // Do not call any stack command before receiving this boot event!
+    // ---------------------------------------------------------------------------------------------------------------------------
+    // This event indicates the device has started & the radio is ready. Do not call any stack cmd before receiving this boot event!
     case sl_bt_evt_system_boot_id:
       // Print boot message.
       app_log_info("Bluetooth stack booted: v%d.%d.%d-b%d\n",
@@ -126,10 +132,7 @@ void sl_bt_on_event(sl_bt_msg_t *evt)
       system_id[6] = address.addr[1];
       system_id[7] = address.addr[0];
 
-      sc = sl_bt_gatt_server_write_attribute_value(gattdb_system_id,
-                                                   0,
-                                                   sizeof(system_id),
-                                                   system_id);
+      sc = sl_bt_gatt_server_write_attribute_value(gattdb_system_id, 0, sizeof(system_id), system_id);
       app_assert_status(sc);
 
       app_log_info("Bluetooth %s address: %02X:%02X:%02X:%02X:%02X:%02X\n",
@@ -146,47 +149,42 @@ void sl_bt_on_event(sl_bt_msg_t *evt)
       app_assert_status(sc);
 
       // Set advertising interval to 100ms.
-      sc = sl_bt_advertiser_set_timing(
-        advertising_set_handle, // advertising set handle
-        160, // min. adv. interval (milliseconds * 1.6)
-        160, // max. adv. interval (milliseconds * 1.6)
-        0,   // adv. duration
-        0);  // max. num. adv. events
+      sc = sl_bt_advertiser_set_timing(advertising_set_handle, 				/* advertising set handle								*/
+    		  	  	  	  	  	  	   160, 								/* min. adv. interval (milliseconds * 1.6)				*/
+        	  	  	  	  	  	  	   160, 								/* max. adv. interval (milliseconds * 1.6)				*/
+        	  	  	  	  	  	  	   0,   								/* adv. duration										*/
+        	  	  	  	  	  	  	   0);  								/* max. num. adv. events								*/
       app_assert_status(sc);
+
       // Start general advertising and enable connections.
-      sc = sl_bt_advertiser_start(
-        advertising_set_handle,
-        sl_bt_advertiser_general_discoverable,
-        sl_bt_advertiser_connectable_scannable);
+      sc = sl_bt_advertiser_start(advertising_set_handle, sl_bt_advertiser_general_discoverable, sl_bt_advertiser_connectable_scannable);
       app_assert_status(sc);
+
       app_log_info("Started advertising\n");
+
       break;
 
-    // -------------------------------
+    // ---------------------------------------------------------------------------------------------------------------------------
     // This event indicates that a new connection was opened.
     case sl_bt_evt_connection_opened_id:
       app_log_info("Connection opened\n");
 
 #ifdef SL_CATALOG_BLUETOOTH_FEATURE_POWER_CONTROL_PRESENT
       // Set remote connection power reporting - needed for Power Control
-      sc = sl_bt_connection_set_remote_power_reporting(
-        evt->data.evt_connection_opened.connection,
-        sl_bt_connection_power_reporting_enable);
+      sc = sl_bt_connection_set_remote_power_reporting(evt->data.evt_connection_opened.connection, sl_bt_connection_power_reporting_enable);
       app_assert_status(sc);
-#endif // SL_CATALOG_BLUETOOTH_FEATURE_POWER_CONTROL_PRESENT
+#endif 																		/* SL_CATALOG_BLUETOOTH_FEATURE_POWER_CONTROL_PRESENT	*/
 
       break;
 
-    // -------------------------------
+    // ---------------------------------------------------------------------------------------------------------------------------
     // This event indicates that a connection was closed.
     case sl_bt_evt_connection_closed_id:
       app_log_info("Connection closed\n");
       // Restart advertising after client has disconnected.
-      sc = sl_bt_advertiser_start(
-        advertising_set_handle,
-        sl_bt_advertiser_general_discoverable,
-        sl_bt_advertiser_connectable_scannable);
+      sc = sl_bt_advertiser_start(advertising_set_handle, sl_bt_advertiser_general_discoverable, sl_bt_advertiser_connectable_scannable);
       app_assert_status(sc);
+
       app_log_info("Started advertising\n");
       break;
 
@@ -194,51 +192,57 @@ void sl_bt_on_event(sl_bt_msg_t *evt)
     // Add additional event handlers here as your application requires!      //
     ///////////////////////////////////////////////////////////////////////////
 
-    // -------------------------------
+    // ---------------------------------------------------------------------------------------------------------------------------
     // Default event handler.
     default:
       break;
   }
+
+  return;
 }
 
-/**************************************************************************//**
+
+/*********************************************************************************************************************************//**
  * Callback function of connection close event.
  *
  * @param[in] reason Unused parameter required by the health_thermometer component
  * @param[in] connection Unused parameter required by the health_thermometer component
- *****************************************************************************/
-void sl_bt_connection_closed_cb(uint16_t reason, uint8_t connection)
-{
+ ************************************************************************************************************************************/
+void sl_bt_connection_closed_cb(uint16_t reason, uint8_t connection) {
+
+  //Locals
   (void)reason;
   (void)connection;
   sl_status_t sc;
 
+
   // Stop timer.
   sc = sl_simple_timer_stop(&app_periodic_timer);
   app_assert_status(sc);
+
+  return;
 }
 
-/**************************************************************************//**
+
+/*********************************************************************************************************************************//**
  * Health Thermometer - Temperature Measurement
  * Indication changed callback
  *
  * Called when indication of temperature measurement is enabled/disabled by
  * the client.
- *****************************************************************************/
-void sl_bt_ht_temperature_measurement_indication_changed_cb(uint8_t connection,
-                                                            sl_bt_gatt_client_config_flag_t client_config)
-{
+ ************************************************************************************************************************************/
+void sl_bt_ht_temperature_measurement_indication_changed_cb(uint8_t connection, sl_bt_gatt_client_config_flag_t client_config) {
+  //Locals
   sl_status_t sc;
   app_connection = connection;
+
+
   // Indication or notification enabled.
   if (sl_bt_gatt_disable != client_config) {
     // Start timer used for periodic indications.
-    sc = sl_simple_timer_start(&app_periodic_timer,
-                               SL_BT_HT_MEASUREMENT_INTERVAL_SEC * 1000,
-                               app_periodic_timer_cb,
-                               NULL,
-                               true);
+    sc = sl_simple_timer_start(&app_periodic_timer, SL_BT_HT_MEASUREMENT_INTERVAL_SEC * 1000, app_periodic_timer_cb, NULL, true);
     app_assert_status(sc);
+
     // Send first indication.
     app_periodic_timer_cb(&app_periodic_timer, NULL);
   }
@@ -247,15 +251,18 @@ void sl_bt_ht_temperature_measurement_indication_changed_cb(uint8_t connection,
     // Stop timer used for periodic indications.
     (void)sl_simple_timer_stop(&app_periodic_timer);
   }
+
+  return;
 }
 
-/**************************************************************************//**
+
+/*********************************************************************************************************************************//**
  * Simple Button
  * Button state changed callback
  * @param[in] handle Button event handle
- *****************************************************************************/
-void sl_button_on_change(const sl_button_t *handle)
-{
+ ************************************************************************************************************************************/
+void sl_button_on_change(const sl_button_t *handle) {
+
   // Button pressed.
   if (sl_button_get_state(handle) == SL_SIMPLE_BUTTON_PRESSED) {
     if (&sl_button_btn0 == handle) {
@@ -270,24 +277,32 @@ void sl_button_on_change(const sl_button_t *handle)
       app_btn0_pressed = false;
     }
   }
+
+  return;
 }
 
-/**************************************************************************//**
+
+/*********************************************************************************************************************************//**
  * Timer callback
  * Called periodically to time periodic temperature measurements and indications.
- *****************************************************************************/
-static void app_periodic_timer_cb(sl_simple_timer_t *timer, void *data)
-{
+ ************************************************************************************************************************************/
+static void app_periodic_timer_cb(sl_simple_timer_t *timer, void *data) {
+
+  //Locals
   (void)data;
   (void)timer;
   sl_status_t sc;
   int32_t temperature = 0;
   uint32_t humidity = 0;
   float tmp_c = 0.0;
-  // float tmp_f = 0.0;
+#ifdef USING_FAHRENHEIT
+  float tmp_f = 0.0;
+#endif
+
 
   // Measure temperature; units are % and milli-Celsius.
   sc = sl_sensor_rht_get(&humidity, &temperature);
+
   if (sc != SL_STATUS_OK) {
     app_log_warning("Invalid RHT reading: %lu %ld\n", humidity, temperature);
   }
@@ -299,30 +314,39 @@ static void app_periodic_timer_cb(sl_simple_timer_t *timer, void *data)
 
   tmp_c = (float)temperature / 1000;
   app_log_info("Temperature: %5.2f C\n", tmp_c);
+
   // Send temperature measurement indication to connected client.
-  sc = sl_bt_ht_temperature_measurement_indicate(app_connection,
-                                                 temperature,
-                                                 false);
+  sc = sl_bt_ht_temperature_measurement_indicate(app_connection, temperature, false);
+
   // Conversion to Fahrenheit: F = C * 1.8 + 32
   // tmp_f = (float)(temperature*18+320000)/10000;
   // app_log_info("Temperature: %5.2f F\n", tmp_f);
   // Send temperature measurement indication to connected client.
-  // sc = sl_bt_ht_temperature_measurement_indicate(app_connection,
-  //                                                (temperature*18+320000)/10,
-  //                                                true);
+  // sc = sl_bt_ht_temperature_measurement_indicate(app_connection, (temperature*18+320000)/10, true);
   if (sc) {
     app_log_warning("Failed to send temperature measurement indication\n");
   }
+
+  return;
 }
 
+
 #ifdef SL_CATALOG_CLI_PRESENT
-void hello(sl_cli_command_arg_t *arguments)
-{
+/*********************************************************************************************************************************//**
+ * Hello Something
+ * Not really sure this was just here.
+ ************************************************************************************************************************************/
+void hello(sl_cli_command_arg_t *arguments) {
+
+  //Locals
   (void) arguments;
   bd_addr address;
   uint8_t address_type;
   sl_status_t sc = sl_bt_system_get_identity_address(&address, &address_type);
+
+
   app_assert_status(sc);
+
   app_log_info("Bluetooth %s address: %02X:%02X:%02X:%02X:%02X:%02X\n",
                address_type ? "static random" : "public device",
                address.addr[5],
@@ -331,5 +355,8 @@ void hello(sl_cli_command_arg_t *arguments)
                address.addr[2],
                address.addr[1],
                address.addr[0]);
+
+  return;
 }
-#endif // SL_CATALOG_CLI_PRESENT
+#endif 																		/* SL_CATALOG_CLI_PRESENT								*/
+
